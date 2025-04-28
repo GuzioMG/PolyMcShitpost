94a1f65d46c8f60a27a8aeaefc5763a6a13d9b2b
Patbox <patbox4@outlook.com>
2025-04-28 12:39:02 +0200

    Fix TradedItem not working

--- src/main/java/io/github/theepicblock/polymc/mixins/item/TradedItemPolyImplementationMixin.java
+++ src/main/java/io/github/theepicblock/polymc/mixins/item/TradedItemPolyImplementationMixin.java
@@ -2,6 +2,7 @@ package io.github.theepicblock.polymc.mixins.item;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.theepicblock.polymc.impl.Util;
+import io.github.theepicblock.polymc.impl.misc.ComponentChangesMap;
 import io.github.theepicblock.polymc.impl.misc.TransformingPacketCodec;
 import io.github.theepicblock.polymc.impl.mixin.ItemLocationStaticHack;
 import net.minecraft.network.RegistryByteBuf;
@@ -19,8 +20,9 @@ public class TradedItemPolyImplementationMixin {
         return TransformingPacketCodec.encodeOnly(original, (buf, tradedItem) -> {
             var ctx = PacketContext.get();
             var map = Util.tryGetPolyMap(ctx.getClientConnection());
-            var stack = map.getClientItem(tradedItem.itemStack(), ctx.getPlayer(), ItemLocationStaticHack.location.get());
-            return new TradedItem(stack.getItem().getRegistryEntry(), stack.getCount(), ComponentMapPredicate.of(stack.getComponents()));
+            var ogStack = tradedItem.itemStack();
+            var stack = map.getClientItem(ogStack, ctx.getPlayer(), ItemLocationStaticHack.location.get());
+            return stack != ogStack ? new TradedItem(stack.getItem().getRegistryEntry(), stack.getCount(), ComponentMapPredicate.of(new ComponentChangesMap(stack.getComponentChanges()))) : tradedItem;
         });
     }
 }
