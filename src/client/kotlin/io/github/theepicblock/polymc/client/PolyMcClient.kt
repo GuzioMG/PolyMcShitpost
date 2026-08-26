dc7ae2b32634684533e065420ca50a481f5f6b66
Guzio <guzekk@protonmail.com>
2026-08-27 00:09:28 +0200

    stitched this together with the template for a new-version fabric mod, what could POSSIBLY go wrong?

---
+++ src/client/kotlin/io/github/theepicblock/polymc/client/PolyMcClient.kt
@@ -0,0 +1,9 @@
+package io.github.theepicblock.polymc.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object PolyMcClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/io/github/theepicblock/polymc/client/PolyMcDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.github.theepicblock.polymc.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object PolyMcDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
